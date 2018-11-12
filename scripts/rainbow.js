4a81c56e6744b7c9500c2fcba49b260fb170626b
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-11-12 14:15:24 -0500

    rainbow

---
+++ scripts/rainbow.js
@@ -0,0 +1,6 @@
+function sendToRainbow(){
+  var text = document.getElementById("text").value;
+  // var th = document.getElementById("rainbowText");
+  document.getElementById("rainbowText").innerHTML = text;
+  // alert(text);
+}
