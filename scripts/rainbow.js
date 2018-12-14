e5b5b7ac4962867c2112f5514f6e2dbc5ab853bd
Corvin Terzo <cterzo@ucvts.tec.nj.us>
2018-12-14 14:09:32 -0500

    x

--- scripts/rainbow.js
+++ scripts/rainbow.js
@@ -1,6 +1,13 @@
 function sendToRainbow(){
+  var audio = new Audio('videos/crab.mp3');
+
   var text = document.getElementById("text").value;
   // var th = document.getElementById("rainbowText");
+  luketest = text.toLowerCase();
+  console.log(luketest);
+  if (luketest === "luke is gone") {
+    audio.play();
+  }
   document.getElementById("rainbowText").innerHTML = text;
   // alert(text);
 }
